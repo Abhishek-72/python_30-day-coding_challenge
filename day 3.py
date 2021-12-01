529f20fb852baceda1f03faef12dae549a72b732
Abhishek Tripathi <72148523+Abhishek-72@users.noreply.github.com>
2021-12-01 16:44:16 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,6 @@
+two_digit_num = input("enter two digit num ")
+print(type(two_digit_num))
+first_digit_num = two_digit_num[0]
+second_digit_num = two_digit_num[1]
+result = int(first_digit_num) + int(second_digit_num)
+print(result)
