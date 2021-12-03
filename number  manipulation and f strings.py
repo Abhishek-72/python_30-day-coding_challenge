778760c1d6e990aa6a74f783f54ecda7be43c252
Abhishek Tripathi <72148523+Abhishek-72@users.noreply.github.com>
2021-12-03 16:40:02 +0530

    Add files via upload

---
+++ number  manipulation and f strings.py
@@ -0,0 +1,11 @@
+print(int(8/3))
+print(round(8/3,2))
+print(round(3.42452456, 3))
+result = 4 / 2
+result /= 2
+print(result)
+score = 0
+score += 1
+height = 1.8
+iswinning = True
+print(f"your score is {score}, your height is {height}, you are winning is {iswinning}")
