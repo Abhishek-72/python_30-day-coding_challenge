a7566c3f91ae507a6905962524644216b5910c63
Abhishek Tripathi <72148523+Abhishek-72@users.noreply.github.com>
2021-12-09 12:40:11 +0530

    Add files via upload

---
+++ if and else.py
@@ -0,0 +1,6 @@
+print("Welcom to the rollercoaster ")
+height = int(input("what si your height in cm "))
+if height >= 120:
+  print("you can ride the rollercoaster! ")
+else:
+  print("sorry, you have to grow taller before you can risde. ")
